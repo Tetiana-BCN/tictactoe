dfcaaa1bcd3a45a99fcc8ad457035d074a461049
SiGu <miskybox@gmail.com>
2025-03-13 09:20:07 +0100

    feature-board

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,7 @@ public class TicTacToe {
     private static char[][] board;
     private static char currentPlayer;
     private static boolean gameOver;
-    private static Scanner scanner = new Scanner(System.in); 
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         initializeGame();
@@ -17,6 +17,29 @@ public class TicTacToe {
         }
 
         displayFinalResult();
-        scanner.close(); 
-    }  
+        scanner.close();
+    }
+}
+
+private static void initializeGame() {
+    board = new char[3][3];
+    currentPlayer = 'X';
+    gameOver = false;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = '-';
+        }
+    }
+}
+
+private static void displayBoard() {
+    System.out.println("\n-------------");
+    for (int i = 0; i < 3; i++) {
+        System.out.print("| ");
+        for (int j = 0; j < 3; j++) {
+            System.out.print(board[i][j] + " | ");
+        }
+        System.out.println("\n-------------");
+    }
+    System.out.println();
 }
